e9c2a0d98a2faa8e8d90e7699242e83add220327
bertco <albert.kashipov@gmail.com>
2019-07-14 14:39:13 +0300

    добавил в create.sh команды по заданию 3 и 4. Урок 1

--- create.sh
+++ create.sh
@@ -11,3 +11,17 @@ CREATE DATABASE example;
 
 # создание таблице users из двух столбцов числового id и текстового name
 CREATE TABLE users(id INT, name TEXT);
+
+# Задание 3. Урок 1.
+# создание дампа БД example и разворот содержимого дампа в новую базу данных sample
+mysqldump example > sample.sql
+
+CREATE DATABASE sample;
+
+sample < sample.sql
+
+# Задание 4. Урок 1.
+# Создайте дамп единственной таблицы help_keyword базы данных mysql. Причем добейтесь того,
+# чтобы дамп содержал только первые 100 строк таблицы.
+
+mysqldump mysql help_keyword WHER ='TRUE LIMIT 100' > help_keyword_dump.sql
